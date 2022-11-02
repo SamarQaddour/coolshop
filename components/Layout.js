f8f3091bce16a78d1d49a4c36625e973564c926b
unknown <samarkaddour589@gmail.com>
2022-11-02 13:00:19 +0200

    Added my project

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,44 @@
 import React from "react"
 import Head from "next/head"
 import NextLink from "next/link"
-import { AppBar, Container, Typography, Toolbar, Link } from "@material-ui/core"
+import {
+  AppBar,
+  Container,
+  Typography,
+  Toolbar,
+  Link,
+  createMuiTheme,
+  ThemeProvider,
+  CssBaseline
+} from "@material-ui/core"
 import useStyles from "../utils/styles"
 export default function Layout({ children, title, description }) {
+  const theme = createMuiTheme({
+    typography: {
+      h1: {
+        fontSize: "1.6rem",
+        fontWeight: 400,
+        margin: "1rem 0"
+      },
+      h2: {
+        fontSize: "1.4rem",
+        fontWeight: 400,
+        margin: "1rem 0"
+      },
+      body1: {
+        fontWeight: "normal"
+      }
+    },
+    palette: {
+      type: "light",
+      primary: {
+        main: "#f0c000"
+      },
+      secondary: {
+        main: "#208080"
+      }
+    }
+  })
   const classes = useStyles()
   return (
     <div>
@@ -11,19 +46,22 @@ export default function Layout({ children, title, description }) {
         <title> {title ? `${title} - Next Amazona` : "Next Amazona"}</title>
         {description && <meta name="description" content={description}></meta>}
       </Head>
-      <AppBar position="static" className={classes.navbar}>
-        <Toolbar>
-          <NextLink href="/" passHerf>
-            <Link>
-              <Typography className={classes.brand}>Amazona</Typography>
-            </Link>
-          </NextLink>
-        </Toolbar>
-      </AppBar>
-      <Container className={classes.main}>{children}</Container>
-      <footer className={classes.footer}>
-        All rights reserved.next Amazona
-      </footer>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="static" className={classes.navbar}>
+          <Toolbar>
+            <NextLink href="/" passHerf>
+              <Link>
+                <Typography className={classes.brand}>Amazona</Typography>
+              </Link>
+            </NextLink>
+          </Toolbar>
+        </AppBar>
+        <Container className={classes.main}>{children}</Container>
+        <footer className={classes.footer}>
+          All rights reserved.next Amazona
+        </footer>
+      </ThemeProvider>
     </div>
   )
 }
